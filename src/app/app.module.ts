80a3c91feaa986eb65083427af5c358f3b63ac88
Ayush Shashank <ayush.bibhas@gmail.com>
2021-04-13 22:52:52 +0530

    package update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginAdminComponent } from './login-admin/login-admin.component';
 import { HomeAdminComponent } from './home-admin/home-admin.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AddEmpComponent } from './add-emp/add-emp.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AccordionModule } from 'ngx-bootstrap/accordion';
 import { EditEmpComponent } from './edit-emp/edit-emp.component';
 import { ViewEmpComponent } from './view-emp/view-emp.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginAdminComponent, HomeAdminComponent, AddEmpComponent, EditEmpComponent, ViewEmpComponent],
+  declarations: [
+    AppComponent,
+    LoginAdminComponent,
+    HomeAdminComponent,
+    AddEmpComponent,
+    EditEmpComponent,
+    ViewEmpComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule,
-    AccordionModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
